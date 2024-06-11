aee72e9d4dd865fdf8e94bf56b803e04109d40f4
MiguelDonado <miguel.5799@hotmail.es>
2024-06-11 09:07:01 +0200

    Fix some regex

--- pliegopdf.py
+++ pliegopdf.py
@@ -115,13 +115,13 @@ def is_price_on_paragraph(paragraph):
 
 
 list_of_lands = get_pliego_relevant_info(
-    "https://www.hacienda.gob.es/DGPatrimonio/Gesti%C3%B3n%20Patrimonial/subastas/DEH-SEVILLA/SUBASTA%2025-06-2024/PLIEGO%20CONDICIONES%20DECIMOQUINTA%20SUBASTA%20%20ocho%20inmuebles%20SUBASTA%2025%20junio%202024.pdf.xsig.pdf"
+    "https://www.hacienda.gob.es/DGPatrimonio/Gesti%C3%B3n%20Patrimonial/subastas/DEH%20TARRAGONA/PLIEGO%20DE%20CONDICIONES.pdf"
 )
 for counter, info in enumerate(list_of_lands):
     print(f"The {counter+1} has the next info: {info}")
 
 """ print(
     read_pdf(
-        "https://www.hacienda.gob.es/DGPatrimonio/Gesti%C3%B3n%20Patrimonial/subastas/DEH-SEVILLA/SUBASTA%2025-06-2024/PLIEGO%20CONDICIONES%20DECIMOQUINTA%20SUBASTA%20%20ocho%20inmuebles%20SUBASTA%2025%20junio%202024.pdf.xsig.pdf"
+        "https://www.hacienda.gob.es/DGPatrimonio/Gesti%C3%B3n%20Patrimonial/subastas/DEH%20TARRAGONA/PLIEGO%20DE%20CONDICIONES.pdf"
     )
 ) """
--- support_regex.py
+++ support_regex.py
@@ -3,7 +3,7 @@ import re
 auction_href_pattern = re.compile("^https://.+Estado/Paginas/Subastas/.+")
 # The first segment of the regex is for "fincas rusticas", the second for "fincas urbanas", the third is for a strange case
 ref_catastral_pattern = re.compile(
-    r"^(?!.*(?:sur|norte|oeste|este|;6)).*Referen\s?cias?\s+Catastral(?:es)?(?:\ses\sla)?:?\s+(\d{2}\d{3}[A-Z]\d{3}\d{5}\d{4}[A-Z]{2}|\d{7}[A-Z]{2}\d{4}[A-Z]\d{4}[A-Z]{2}|\d{7}[A-Z]{2}\d{4}[A-Z])",
+    r"^(?!.*(?:sur|norte|oeste|este)).*Referen\s?cias?\s+(?:Catastral)?(?:es)?(?:\ses\sla)?:?\s*(\d{2}\d{3}[A-Z]\d{3}\d{5}\d{4}[A-Z]{2}|\d{7}[A-Z]{2}\d{4}[A-Z]\d{4}[A-Z]{2}|\d{7}[A-Z]{2}\d{4}[A-Z])",
     flags=re.MULTILINE | re.IGNORECASE,
 )
 
