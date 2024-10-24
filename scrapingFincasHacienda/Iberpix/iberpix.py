f1d0787c9a006e5368e1a4e1bbc2482d1ef0f617
MiguelDonado <miguel.5799@hotmail.es>
2024-10-24 10:39:34 +0200

    Remove and add some functionality to the iberpix.py - Create method add land kml layer. - Remove method seach land since is no longer needed because of the kml. - Add time.sleep on my_get_screenshot to let time to load the maps. - Dealing with method 'scrape_usos_suelo_info', habing the usos map in the background, to click on the land to extract more info.

--- scrapingFincasHacienda/Iberpix/iberpix.py
+++ scrapingFincasHacienda/Iberpix/iberpix.py
@@ -3,11 +3,12 @@ from pathlib import Path
 
 import Iberpix.constants as const
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 
 
 class Iberpix(webdriver.Chrome):
-    def __init__(self, delegation, lote, land, ref):
+    def __init__(self, delegation, lote, land, ref, kml_path):
         options = webdriver.ChromeOptions()
         # Keep the browser open after the WebDriver session is terminated.
         options.add_experimental_option("detach", True)
@@ -31,16 +32,19 @@ class Iberpix(webdriver.Chrome):
         self.lote = lote
         self.land = land
         self.ref = ref
+        self.kml_path = kml_path
 
     def get_data(self):
         self.__land_first_page()
-        self.__search_land()
+        self.__hide_menu()
+
+        self.__add_land_kml_layer()
 
         # Curvas de nivel
         filename_mapa_curvas_nivel = f"{self.ref}{const.SUFFIX_FILENAME_CURVAS_NIVEL}"
         self.__my_get_screenshot(filename_mapa_curvas_nivel)
 
-        # Lidar
+        # # Lidar
         self.__show_lidar_map()
         filename_mapa_lidar = f"{self.ref}{const.SUFFIX_FILENAME_LIDAR}"
         self.__my_get_screenshot(filename_mapa_lidar)
@@ -49,42 +53,38 @@ class Iberpix(webdriver.Chrome):
         self.__show_usos_suelo_map()
         filename_mapa_usos_suelo = f"{self.ref}{const.SUFFIX_FILENAME_USOS_SUELO}"
         self.__my_get_screenshot(filename_mapa_usos_suelo)
+        self.__scrape_usos_suelo_info()
 
     def __land_first_page(self):
         self.get(const.BASE_URL_IBERPIX)
 
-    def __search_land(self):
-        parcela_catastro_button = self.find_element(
-            By.XPATH, "//div[@id='m-locator-infocatastro']"
+    def __hide_menu(self):
+        hide_btn = self.find_element(By.XPATH, "//div[@id='m-locator-ignsearch']")
+        hide_btn.click()
+
+    # It also center my land on the middle of the map
+    def __add_land_kml_layer(self):
+        expand_menu_layers_btn = self.find_element(
+            By.XPATH, "//button[@aria-label='Plugin Layerswitcher']"
         )
-        parcela_catastro_button.click()
+        expand_menu_layers_btn.click()
 
-        catastro_button = self.find_element(
-            By.XPATH, "//label[@id='tab-search-catastro']"
+        add_layers_btn = self.find_element(
+            By.XPATH, "//button[@id='m-layerswitcher-addlayers']"
         )
-        catastro_button.click()
+        add_layers_btn.click()
 
-        input_search_ref_catastral = self.find_element(
-            By.XPATH, "//input[@id='m-refCatastral-input']"
+        time.sleep(3)
+
+        upload_file_input = self.find_element(
+            By.XPATH, "//input[@id='m-layerswitcher-addservices-file-input']"
         )
-        input_search_ref_catastral.send_keys(self.ref)
+        upload_file_input.send_keys(self.kml_path)
 
-        search_btn = self.find_element(
-            By.XPATH, "//button[@id='m-infocatastro-refCatastral']"
+        hide_side_bar_btn = self.find_element(
+            By.XPATH, "//button[@aria-label='Plugin Layerswitcher']"
         )
-        search_btn.click()
-
-        # To hide the pop-up, so we can see entirely the map
-        parcela_catastro_button.click()
-
-        # Close a popup that shows a bit the info about the land
-
-        close_btn = self.find_element(
-            By.XPATH,
-            '//div[div[b[text()="Dirección exacta"]]]/preceding-sibling::div/a[@title="cerrar popup"]',
-        )
-
-        close_btn.click()
+        hide_side_bar_btn.click()
 
     def __show_lidar_map(self):
         choose_background_layer = self.find_element(
@@ -116,7 +116,32 @@ class Iberpix(webdriver.Chrome):
         )
         close_side_panel.click()
 
+    def __scrape_usos_suelo_info(self):
+        # To have the mouse on info mode, so i can click on the land in the map and get extra information
+        info_mode_btn = self.find_element(By.XPATH, "//button[@id='m-information-btn']")
+        info_mode_btn.click()
+
+        time.sleep(5)
+        # Get the size of the map element
+        map_element = self.find_element(By.XPATH, "//canvas")
+        map_size = map_element.size
+        map_width = map_size["width"]
+        map_height = map_size["height"]
+
+        # Calculate the center point of the map
+        center_x = map_width / 2
+        center_y = map_height / 2
+
+        actions = ActionChains(self)
+
+        # ITS NOT WORKING !!!!
+        # actions.move_to_element_with_offset(map_element, center_x, center_y).click().perform()
+
     def __my_get_screenshot(self, filename):
+        # The background layers takes a bit of time to load. If i dont set any time, the screenshot
+        # is wrong, and i see nothing
+        time.sleep(1)
+
         # Build the filename that will have our downloader file
         path = const.DOWNLOAD_DIR / filename
 
